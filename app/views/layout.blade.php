96a04d6671a0cd148729d346b609beeaf6f4a83d
andres pinzon <andresmaopinzon@gmail.com>
2015-01-14 19:38:53 -0500

    Languaje google

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -13,6 +13,7 @@
     <meta name="author" content="andrestntx">
     <meta name="robots" content="index, follow">
 	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1.0">
+    <link rel=”alternate” hreflang="co" href="http://alba.boutique" />
 	{{--End Metas--}}
 
 	{{-- Icons --}}
