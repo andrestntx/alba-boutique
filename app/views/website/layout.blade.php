0a98e989c001def75163d5fba8fa2d4c23181b8e
andres pinzon <andresmaopinzon@gmail.com>
2015-01-22 12:32:49 -0500

    Remarketing google adwords

--- app/views/website/layout.blade.php
+++ app/views/website/layout.blade.php
@@ -139,5 +139,20 @@
         src="https://apis.google.com/js/platform.js?publisherid=106563667879480455749">
     </script>
 
+    <script type="text/javascript">
+        /* <![CDATA[ */
+        var google_conversion_id = 960142514;
+        var google_custom_params = window.google_tag_params;
+        var google_remarketing_only = true;
+        /* ]]> */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
+    <noscript>
+        <div style="display:inline;">
+            <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/960142514/?value=0&amp;guid=ON&amp;script=0"/>
+        </div>
+    </noscript>
+
+
     @yield('extra-js')
 @stop
