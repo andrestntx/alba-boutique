0aac4d0ebd0b22919644dc7ba7a991a2c08ae544
andres pinzon <andresmaopinzon@gmail.com>
2015-01-26 18:04:51 -0500

    Precio formateado en el catalogo

--- app/views/website/pages/catalog/category.blade.php
+++ app/views/website/pages/catalog/category.blade.php
@@ -59,7 +59,7 @@
                     <article class="col-md-3 col-sm-4 col-xs-12 thumb">
                         <div class="widget" itemscope itemtype="http://schema.org/Product">
                             <div class="widget-content widget-content-mini themed-background-muted">
-                                <div class="pull-right text-muted"><span itemprop="price"> $ {{ $product->price }} </span></div>
+                                <div class="pull-right text-muted"><span itemprop="price"> $ {{ $product->formated_price }} </span></div>
                                 <i class="fa fa-heart"></i> <span itemprop="name">{{ $product->short_name }} </span>
                             </div>
                             <div class="widget-content text-center">
