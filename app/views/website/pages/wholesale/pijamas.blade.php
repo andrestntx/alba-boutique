af7fe9573ec7c9f02ee3b9639d6d485d0081e72c
andres pinzon <andresmaopinzon@gmail.com>
2015-01-31 10:38:38 -0500

    Analitycs Adwords Pijamas por mayor

--- app/views/website/pages/wholesale/pijamas.blade.php
+++ app/views/website/pages/wholesale/pijamas.blade.php
@@ -134,5 +134,22 @@
     </section>
 @stop
 @section('extra-js')
-
+    <!-- Google Code for Datos Formulario Pijamas por Mayor Conversion Page -->
+    <script type="text/javascript">
+        /* <![CDATA[ */
+        var google_conversion_id = 960142514;
+        var google_conversion_language = "en";
+        var google_conversion_format = "3";
+        var google_conversion_color = "ffffff";
+        var google_conversion_label = "5cp3CJ3mo1kQsrnqyQM";
+        var google_remarketing_only = false;
+        /* ]]> */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+    </script>
+    <noscript>
+        <div style="display:inline;">
+            <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/960142514/?label=5cp3CJ3mo1kQsrnqyQM&amp;guid=ON&amp;script=0"/>
+        </div>
+    </noscript>
 @stop
